df668a14cd9ece50343f2d01e6f7b9beed908a4b
Zaya <zayazaw67@gmail.com>
2019-09-08 18:04:52 -0500

    created target math formula

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,28 @@
-$("#red").on("click", function() {
+$(document).ready(function() {
 
-    alert("clicked the red gem.");})
+
+
+$("#objective").text(target)
+    var target = Math.floor(Math.random() * 101) +19 ;
+    console.log(target)
+
+    var random = Math.floor(Math.random() * 12) +1 ;
+    // console.log(random)
+
+
+$("#red").on("click", function () {
+    alert("clicked the red gem.");
+})
+$("#green").on("click", function () {
+    alert("clicked the red gem.");
+})
+$("#blue").on("click", function () {
+    alert("clicked the red gem.");
+})
+$("#yellow").on("click", function () {
+    alert("clicked the red gem.");
+})
+
+
+    }
+)
