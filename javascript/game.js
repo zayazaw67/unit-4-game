d1606b4de255aacca4f6af5bdf55045bda1c82a1
Zaya <zayazaw67@gmail.com>
2019-09-10 09:50:15 -0500

    made win/lose if statement, applied random numbers to each gem per game start.

--- javascript/game.js
+++ javascript/game.js
@@ -1,33 +1,96 @@
 $(document).ready(function() {
-
-
-// 120 max | 19 min
-// (max - min) + min to create specific range
-    var target = Math.floor(Math.random() * 101) + 19 ;
-    console.log(target)
-
-    var random = Math.floor(Math.random() * 12) + 1 ; 
-    // console.log(random)
-
-    // randomTotal = random + random + random + random + random until random <>= target
     
+    
+    // 120 max | 19 min
+    // (max - min) + min to create specific range
+    var target;
+    var redGem; 
+    var greenGem; 
+    var blueGem; 
+    var yellowGem; 
+    var wins = 0;
+    var losses = 0;
 
-$("#red").on("click", function () {
-    alert("clicked the red gem.");
-})
-$("#green").on("click", function () {
-    alert("clicked the red gem.");
-})
-$("#blue").on("click", function () {
-    alert("clicked the red gem.");
-})
-$("#yellow").on("click", function () {
-    random
-    alert("clicked the red gem.");
-})
+    var totalScore = 0;
+    
+    var startGame = function() {
+        target = Math.floor(Math.random() * 101) + 19 ;
+        console.log("in the setScore function: " + target),
+        
+        redGem = Math.floor(Math.random() * 12) + 1 ; 
+        console.log("redGem: " + redGem);
+       
+        greenGem = Math.floor(Math.random() * 12) + 1 ; 
+        console.log("greenGem: " + greenGem);
+        
+        blueGem = Math.floor(Math.random() * 12) + 1 ; 
+        console.log("blueGem: " + blueGem);
+        
+        yellowGem = Math.floor(Math.random() * 12) + 1 ; 
+        console.log("yellowGem: " + yellowGem);
 
-$("#objective").text("Your target is " + target)
-$("#scoreboard").text("put scoreboard here")
-$("#totalscore").text(random)
+        totalScore = 0;
     }
-)
+
+    startGame()
+
+    $("#red").on("click", function () {
+        totalScore += redGem;
+        console.log("Current" + totalScore)
+        if (totalScore > target) {
+            console.log ("lost")
+            losses++;
+            startGame()
+        } else if (totalScore === target) {
+            console.log ("won")
+            wins++;
+            startGame();
+        }
+    })
+    $("#green").on("click", function () {
+        totalScore += greenGem;
+        console.log("Current" + totalScore)
+        if (totalScore > target) {
+            console.log ("lost")
+            losses++;
+            startGame()
+        } else if (totalScore === target) {
+            console.log ("won")
+            wins++;
+            startGame();
+        }
+    })
+
+    $("#blue").on("click", function () {
+        totalScore += blueGem;
+        console.log("Current" + totalScore)
+        if (totalScore > target) {
+            console.log ("lost")
+            losses++;
+            startGame()
+        } else if (totalScore === target) {
+            console.log ("won")
+            wins++;
+            startGame();
+        }
+    })
+    $("#yellow").on("click", function () {
+        totalScore += yellowGem;
+        console.log("Current" + totalScore)
+        if (totalScore > target) {
+            console.log ("lost")
+            losses++;
+            startGame()
+        } else if (totalScore === target) {
+            console.log ("won")
+            wins++;
+            startGame();
+        }
+    })
+        // $(".class")
+
+    
+    $("#objective").text("Your target is " + target)
+    $("#scoreboard").text("Wins: " + wins)
+    $("#scoreboard").text("Losses: " + losses)
+})
